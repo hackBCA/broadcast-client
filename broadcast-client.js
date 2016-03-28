69f287f9b1835d2eea7a47eaf6c398e501a38f04
Robert Kim <rkty13@gmail.com>
2016-03-28 12:24:42 -0400

    Added functionality to send message.

--- broadcast-client.js
+++ broadcast-client.js
@@ -1,4 +1,10 @@
 (function(exports) {
+
+    var socket;
+    var messagesContainer;
+    var textInputEl;
+    var submitButtonEl;
+
     var generateMessage = function (id, sender_name, message, timestamp) {
         var messageSpan = document.createElement("span");
         messageSpan.className = "message";
@@ -15,31 +21,57 @@
         return container;
     }
 
-    var publishMessage = function (messages_container, data) {
+    var publishMessage = function (data) {
         if (!document.getElementById(data.id)) {
             var message = generateMessage(data.id, data.sender_name, data.message, data.timestamp);
-            messages_container.appendChild(message);
+            messagesContainer.appendChild(message);
         }
     };
 
-    var socket_init = function(socket, messages_container) {
+    var socketInit = function() {
         socket.on("connect", function() {
             socket.emit("fetch_all_broadcasts", function(data) {
                 for (var i = 0; i < data.length; i++) {
-                    publishMessage(messages_container, data[i]);
+                    publishMessage(data[i]);
                 }
             });
 
             socket.on("receive_message", function(data) {
-                publishMessage(messages_container, data);
+                publishMessage(data);
             });
         });
     };
 
-    exports.init = function(url, container) {
-        var socket = io.connect(url);
-        var messages_container = document.querySelector(container);
-        socket_init(socket, messages_container);
+    var buttonInit = function () {
+        submitButtonEl.onclick = function () {
+            exports.broadcast();
+        }
+    };
+
+    var sendMessage = function (message) {
+        socket.emit("send_message", message, function (data) {
+            console.log(data);
+        });
+    };
+
+    exports.broadcast = function () {
+        var message = textInputEl.value;
+        sendMessage(message);
+    };
+
+    exports.broadcastWithMessage = function (message) {
+        sendMessage(message);
+    };
+
+    exports.init = function (url, container, textInput, submitButton) {
+        socket = io.connect(url);
+        messagesContainer = document.querySelector(container);
+        textInputEl = document.querySelector(textInput);
+        submitButtonEl = document.querySelector(submitButtonEl);
+        if (submitButtonEl) {
+            buttonInit();
+        }
+        socketInit();
     };
 
 })(this.BroadcastClient = {});
